add027122e0a6ae2496d0d364f1a6b3b9a796315
Baptiste Fernandez <baptiste.fernandez.euronews@gmail.com>
2019-07-24 11:52:57 +0200

    :tada: next-page script + test index

---
+++ main.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const PARAM_REGEX = /(\?|\&)(\w+=([\d]+))/;
+const LISTENED_MOUSE_BTN = 2;
+
+function digits(number, count = 1) {
+    return number >= 10 ? digits(number / 10, count + 1) : count;
+}
+
+function nextPageUrl([full, sign, param, result]) {
+    return window.location.href.replace(param, param.substr(0, param.length - digits(result)) + (parseInt(result) + 1));
+}
+
+function main() {
+    document.body.addEventListener('mousedown', mouseEvent => {
+        if (mouseEvent.which === LISTENED_MOUSE_BTN) {
+            const match = window.location.href.match(PARAM_REGEX);
+            if (match) {
+                window.location = nextPageUrl(match);
+            }
+        }
+    });
+}
---
+++ script.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         Next page script
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  To forward directly unvisited next page
+// @author       FiBoot
+// @match        *
+// @grant        none
+// @require      https://raw.githubusercontent.com/FiBoot/next-page-script/master/main.js
+// ==/UserScript==
+
+main();
